1346c14e150476dfcbebceca508ab7174d6a682a
Babu Macharia <babu.macharia@gmail.com>
2025-03-19 15:17:55 -0700

    pyramid and abs function

---
+++ categories/functions/more_function_practice/pyramid.py
@@ -0,0 +1,16 @@
+
+'''
+Pyramid Function
+Write a function, `pyramid`, that takes a single character and a number as parameters and displays an ASCII art pyramid to the screen with `number` rows.
+ 
+By: Babu Macharia
+
+'''
+
+
+def pyramid():
+  for i in range(1,10):
+    print("*" * i)
+
+pyramid()
+  
